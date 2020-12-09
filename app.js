19347205c82bcd8a1a26bd37984a6d1e8c97f149
quanganh991 <dinhqanh2012@gmail.com>
2020-12-09 13:06:38 +0700

    complete initing all things

--- app.js
+++ app.js
@@ -43,3 +43,107 @@ module.exports = app;
 app.listen(62033, () => {
   console.log('Site running on port 62033');
 });
+
+
+//các đường dẫn để xác định API
+//1.Lấy thông tin toàn bộ bãi xe
+var get_all_station = require('./controller/01_get_all_station/get_all_station.js');
+app.use('/all_station', get_all_station);
+
+
+//2.Lấy thông tin toàn bộ xe trong bãi
+var get_all_bike = require('./controller/02_get_all_bike/get_all_bike');
+app.use('/all_bike', get_all_bike);
+
+
+
+//3.Lấy thông tin detail của bãi xe
+var get_detail_station = require('./controller/03_get_detail_station/get_detail_station');
+app.use('/detail_station/:id_station', get_detail_station);
+
+
+
+//4.Lấy thông tin detail của xe
+var get_detail_bike = require('./controller/04_get_detail_bike/get_detail_bike');
+app.use('/detail_bike/:id_bike', get_detail_bike);
+
+
+
+//5.search thông tin bãi xe
+var search_station = require('./controller/05_search_station_detail/search_station_detail');
+app.use('/search_station/:station_name', search_station);
+
+
+
+//6.Thanh toán
+var pay_before_rent = require('./controller/06_pay_before_rent/pay_before_rent');
+app.use('/pay_before_rent', pay_before_rent);
+
+
+
+//7.
+//8.Thêm mới một bãi xe
+var add_station = require('./controller/08_add_station/add_station');
+app.use('/add_station', add_station);
+
+
+
+//9.Thay đổi thông tin một bãi xe
+var update_station = require('./controller/09_update_station/update_station');
+app.use('/update_station', update_station);
+
+
+
+//10.Xóa bãi xe
+var delete_station = require('./controller/10_delete_station/delete_station');
+app.use('/delete_station', delete_station);
+
+
+
+//11.Thêm mới một xe
+var add_bike = require('./controller/11_add_bike/add_bike');
+app.use('/add_bike', add_bike);
+
+
+
+//12.thay đổi thông tin mỗi xe
+var update_bike = require('./controller/12_update_bike/update_bike');
+app.use('/update_bike', update_bike);
+
+
+
+//13.search search xe
+var search_bike = require('./controller/13_search_bike/search_bike');
+app.use('/search_bike/:bike_name', search_bike);
+
+
+
+//14.Xóa một xe
+var delete_bike = require('./controller/14_delete_bike/delete_bike');
+app.use('/delete_bike', delete_bike);
+
+
+
+//15.lấy thông tin xe thuê từ idXe
+var get_rent_bike_by_id = require('./controller/15_get_rent_bike_by_id/get_rent_bike_by_id');
+app.use('/get_rent_bike_by_id', get_rent_bike_by_id);
+
+
+
+//16.thanh toán xe muốn thuê
+var pay_after_rent = require('./controller/16_pay_after_rent/pay_after_rent');
+app.use('/pay_after_rent', pay_after_rent);
+
+
+
+//17.Lấy thông tin xe đang thuê
+var get_renting_bike_information = require('./controller/17_get_renting_bike_information/get_renting_bike_information');
+app.use('/get_renting_bike_information', get_renting_bike_information);
+
+
+
+//18.Trả xe
+var register_return_bike = require('./controller/18_register_return_bike/register_return_bike');
+app.use('/register_return_bike', register_return_bike);
+
+
---
+++ controller/01_get_all_station/get_all_station.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var readjson = require('../../readjson.js')
+var router = express.Router();
+module.exports = router;
+
+router.get('/', (req, res) => {// /all_station/
+    // var newest = readjson.loadByNewestOption(config.LIMIT_OPTION);
+    //
+    // readTextFile("park.json", function (text) {
+    //                 document.writeln(text);
+    //             });
+    //
+});
