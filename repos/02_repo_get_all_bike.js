06fcfa67a60621c38668a507881329276374583b
quanganh991 <dinhqanh2012@gmail.com>
2020-12-21 16:27:16 +0700

    API 2: Get Bike only

--- repos/02_repo_get_all_bike.js
+++ repos/02_repo_get_all_bike.js
@@ -1,5 +1,5 @@
 var db = require('../fn/db');
 exports.getAllBike = (id_station) => {
-    var sql = "select * from bike WHERE idPark = '" + id_station.toString() + "'";
+    var sql = "select * from bike WHERE idPark = '" + id_station.toString() + "' and type = 'bike'";
     return db.load(sql);
 }
