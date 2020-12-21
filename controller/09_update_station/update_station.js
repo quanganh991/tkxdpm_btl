378012a5a71121199eb3f7028cdd8558dd4e0b64
quanganh991 <dinhqanh2012@gmail.com>
2020-12-21 11:28:55 +0700

    Sửa station trả về station đã sửa chứ không phải true/false

--- controller/09_update_station/update_station.js
+++ controller/09_update_station/update_station.js
@@ -7,7 +7,16 @@ var updateStation = require('../../repos/09_repo_update_station.js');
 router.post('/', (req, res) => {
     updateStation.updateStation(req.body).then(value => {
         var vm = {
-            result: "true"
+
+                id_park: req.body.id_park,
+                stationName: req.body.stationName,
+                stationAddress: req.body.stationAddress,
+                numberOfBikes: req.body.numberOfBikes,
+                numberOfEBikes: req.body.numberOfEBikes,
+                numberOfTwinBikes: req.body.numberOfTwinBikes,
+                numberOfEmptyDocks: req.body.numberOfEmptyDocks,
+                areaToWalk: req.body.areaToWalk,
+                timeToWalk: req.body.timeToWalk
         };
         // res.render('09_update_station', vm);
         res.set({ 'content-type': 'application/json; charset=utf-8' });
