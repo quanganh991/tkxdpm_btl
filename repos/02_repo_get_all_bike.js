6c44aa17b9bae3c3d6557b0ac87364e91b16fcd8
quanganh991 <dinhqanh2012@gmail.com>
2020-12-21 17:46:19 +0700

    get AllBike but only one type

--- repos/02_repo_get_all_bike.js
+++ repos/02_repo_get_all_bike.js
@@ -1,10 +1,10 @@
 var db = require('../fn/db');
 exports.getAllBike = (id_station) => {
-    var sql = "select * from bike WHERE idPark = " + id_station.toString() + " AND typed = 'ebike'";
+    var sql = "select * from bike WHERE idPark = " + id_station.toString() + " AND typed = 'bike'";
     return db.load(sql);
 }
 
 exports.getAllBikeInAllPark = () => {
-    var sql = "select * from bike AND typed = 'ebike'";
+    var sql = "select * from bike AND typed = 'bike'";
     return db.load(sql);
 }
