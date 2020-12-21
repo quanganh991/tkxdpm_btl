8b028ac8cf43e2dc7fa528f92d5751184055a3f3
quanganh991 <dinhqanh2012@gmail.com>
2020-12-21 11:12:34 +0700

    API chỉ trả về dữ liệu mà ko trả về message

--- controller/json.js
+++ controller/json.js
@@ -6,12 +6,15 @@ module.exports = {
     // | author: pvhau       |
     //  ---------------------
     success: function (req, res, data = '') {
-        res.status(200).send({
-            message: '',
-            data: data,
-            status: 'SUCCESS',
-            code: 200
-        })
+        res.status(200).send(
+        //     {
+        //     message: '',
+        //     data: data,
+        //     status: 'SUCCESS',
+        //     code: 200
+        // }
+            data
+        )
         return 200;
     },
 
@@ -19,16 +22,19 @@ module.exports = {
     // | Config for not found |
     // | url error message    |
     // | author: pvhau       |
-    //  ----------------------   
+    //  ----------------------
 
     notFoundUrl: function (req, res) {
         let message = req.originalUrl + ' not found';
-        res.status(404).send({
-            message: message,
-            data: '',
-            status: 'FAILED',
-            code: 404
-        })
+        res.status(404).send(
+        //     {
+        //     message: message,
+        //     data: '',
+        //     status: 'FAILED',
+        //     code: 404
+        // }
+            data
+        )
         console.log(message);
         elkLog.Error(message, 'Client');
         return message;
