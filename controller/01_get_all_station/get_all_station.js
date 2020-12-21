b560320d8cae3108cad214282c9f3ed499308e2f
quanganh991 <dinhqanh2012@gmail.com>
2020-12-21 19:43:45 +0700

    Fix API 5

--- controller/01_get_all_station/get_all_station.js
+++ controller/01_get_all_station/get_all_station.js
@@ -2,17 +2,36 @@ var express = require('express');
 var router = express.Router();
 var jsonFormat = require('../json.js');
 module.exports = router;
+const querystring = require('querystring');
+//
+// var getAllStationRepo = require('../../repos/01_repo_get_all_station');
+// router.get('/', (req, res) => {// /all_station/
+//     var allParks = getAllStationRepo.getAllStation();
+//     Promise.all([allParks]).then(([all_parks]) => {
+//         // var vm = {
+//         //     allParks: all_parks,
+//         // };
+//         // res.render('01', vm);
+//         res.set({ 'content-type': 'application/json; charset=utf-8' });
+//         // res.end(JSON.stringify(all_parks));
+//         jsonFormat.success(req, res, all_parks);
+//     });
+// });
 
-var getAllStationRepo = require('../../repos/01_repo_get_all_station');
-router.get('/', (req, res) => {// /all_station/
-    var allParks = getAllStationRepo.getAllStation();
-    Promise.all([allParks]).then(([all_parks]) => {
+var searchStationDetailBy2Keyword = require('../../repos/05_repo_search_station_detail.js');
+router.get('', (req, res) => {//
+    var stationName = req.query.stationName;
+    var stationAddress = req.query.stationAddress;
+    console.log("--------------stationName = " + stationName.toString());
+    console.log("--------------stationAddress = " + stationAddress.toString());
+    var allStationMatch = searchStationDetailBy2Keyword.searchStationDetailBy2Keyword(stationName, stationAddress);
+    Promise.all([allStationMatch]).then(([allStationMatch]) => {
         // var vm = {
-        //     allParks: all_parks,
+        // allStationMatch: allStationMatch,
         // };
-        // res.render('01', vm);
+        // res.render('05_search_station_detail'/*.pug*/, vm);
         res.set({ 'content-type': 'application/json; charset=utf-8' });
-        // res.end(JSON.stringify(all_parks));
-        jsonFormat.success(req, res, all_parks);
+        // res.end(JSON.stringify(allStationMatch));
+        jsonFormat.success(req, res, allStationMatch);
     });
 });
--- repos/05_repo_search_station_detail.js
+++ repos/05_repo_search_station_detail.js
@@ -4,3 +4,8 @@ exports.searchStationDetail = (keyword) => {
     var sql = "select * from park where stationName like '%" + keyword + "%' or stationAddress like '%" + keyword + "%'";
     return db.load(sql);
 }
+
+exports.searchStationDetailBy2Keyword = (station, address) => {
+    var sql = "select * from park where stationName like '%" + station + "%' or stationAddress like '%" + address + "%'";
+    return db.load(sql);
+}
