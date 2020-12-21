9c0d003afeb388aee5a59139e02d913463876117
quanganh991 <dinhqanh2012@gmail.com>
2020-12-21 18:17:15 +0700

    3 bike khác nhau cho 3 controller khác nhau

--- controller/02_get_all_bike/get_all_bike.js
+++ controller/02_get_all_bike/get_all_bike.js
@@ -4,6 +4,63 @@ var jsonFormat = require('../json.js');
 module.exports = router;
 
 var getAllBike = require('../../repos/02_repo_get_all_bike.js');
+
+
+router.get('/ebike/:id_station', (req, res) => {// /all_station/
+    var id_station = req.params.id_station;
+    var allBike = getAllBike.getAllEBike(id_station);
+    Promise.all([allBike]).then(([all_bikes]) => {
+        // var vm = {
+        // allBike: all_bikes
+        // };
+        // res.render('02_get_all_bike'/*.pug*/, vm);
+        res.set({ 'content-type': 'application/json; charset=utf-8' });
+        // res.end(JSON.stringify(all_bikes));
+        jsonFormat.success(req, res, all_bikes);
+    });
+});
+
+router.get('/ebike', (req, res) => {// /all_station/
+    var allBikeInAllPark = getAllBike.getAllEBikeInAllPark();
+    Promise.all([allBikeInAllPark]).then(([all_bikesInAllPark]) => {
+        // var vm = {
+        // allBike: all_bikes
+        // };
+        // res.render('02_get_all_bike'/*.pug*/, vm);
+        res.set({ 'content-type': 'application/json; charset=utf-8' });
+        // res.end(JSON.stringify(all_bikes));
+        jsonFormat.success(req, res, all_bikesInAllPark);
+    });
+});
+
+router.get('/twinbike', (req, res) => {// /all_station/
+    var allBike = getAllBike.getAllTwinBikeInAllPark();
+    Promise.all([allBike]).then(([all_bikes]) => {
+        // var vm = {
+        // allBike: all_bikes
+        // };
+        // res.render('02_get_all_bike'/*.pug*/, vm);
+        res.set({ 'content-type': 'application/json; charset=utf-8' });
+        // res.end(JSON.stringify(all_bikes));
+        jsonFormat.success(req, res, all_bikes);
+    });
+});
+
+router.get('/twinbike/:id_station', (req, res) => {// /all_station/
+    var id_station = req.params.id_station;
+    var allBikeInAllPark = getAllBike.getAllTwinBike(id_station);
+    Promise.all([allBikeInAllPark]).then(([all_bikesInAllPark]) => {
+        // var vm = {
+        // allBike: all_bikes
+        // };
+        // res.render('02_get_all_bike'/*.pug*/, vm);
+        res.set({ 'content-type': 'application/json; charset=utf-8' });
+        // res.end(JSON.stringify(all_bikes));
+        jsonFormat.success(req, res, all_bikesInAllPark);
+    });
+});
+
+
 router.get('/:id_station', (req, res) => {// /all_station/
     var id_station = req.params.id_station;
     var allBike = getAllBike.getAllBike(id_station);
@@ -19,8 +76,7 @@ router.get('/:id_station', (req, res) => {// /all_station/
 });
 
 router.get('/', (req, res) => {// /all_station/
-    var id_station = req.params.id_station;
-    var allBikeInAllPark = getAllBike.getAllBikeInAllPark(id_station);
+    var allBikeInAllPark = getAllBike.getAllBikeInAllPark();
     Promise.all([allBikeInAllPark]).then(([all_bikesInAllPark]) => {
         // var vm = {
         // allBike: all_bikes
--- repos/02_repo_get_all_bike.js
+++ repos/02_repo_get_all_bike.js
@@ -5,6 +5,24 @@ exports.getAllBike = (id_station) => {
 }
 
 exports.getAllBikeInAllPark = () => {
-    var sql = "select * from bike AND typed = 'bike'";
+    var sql = "select * from bike WHERE typed = 'bike'";
+    return db.load(sql);
+}
+exports.getAllEBike = (id_station) => {
+    var sql = "select * from bike WHERE idPark = " + id_station.toString() + " AND typed = 'ebike'";
+    return db.load(sql);
+}
+
+exports.getAllEBikeInAllPark = () => {
+    var sql = "select * from bike WHERE typed = 'ebike'";
+    return db.load(sql);
+}
+exports.getAllTwinBike = (id_station) => {
+    var sql = "select * from bike WHERE idPark = " + id_station.toString() + " AND typed = 'twinbike'";
+    return db.load(sql);
+}
+
+exports.getAllTwinBikeInAllPark = () => {
+    var sql = "select * from bike WHERE typed = 'twinbike'";
     return db.load(sql);
 }
