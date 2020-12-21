70177c9068c4c931b022d4b5df57afb7f2bc7c34
quanganh991 <dinhqanh2012@gmail.com>
2020-12-21 10:34:25 +0700

    Bỏ toString

--- repos/11_repo_add_bike.js
+++ repos/11_repo_add_bike.js
@@ -2,30 +2,30 @@ var db = require('../fn/db');
 
 exports.addBike = bike => {
     var sql =
-        bike.type.toString() == "ebike"
+        bike.type == "ebike"
     ?
         "INSERT INTO bike (name, type, weight,licensePlate, manuafacturingDate, producer, cost, idPark,batteryPercentage,loadCycles,timeRemaining) VALUES" +
-        " ('"+ bike.name.toString() +"' ," +
-        "'" + bike.type.toString() +"' ," +
-        "'" + bike.weight.toString() +"' ," +
-        "'" + bike.licensePlate.toString() +"' ," +
-        "'" + bike.manuafacturingDate.toString() +"' ," +
-        "'" + bike.producer.toString() +"' ," +
-        "'" + bike.cost.toString() +"' ," +
-        "'" + bike.idPark.toString() +"' ," +
-        "'" + bike.batteryPercentage.toString() +"' ," +
-        "'" + bike.loadCycles.toString() +"' ," +
-        "'" + bike.timeRemaining.toString() +"' )"
+        " ('"+ bike.name +"' ," +
+        "'" + bike.type +"' ," +
+        "'" + bike.weight +"' ," +
+        "'" + bike.licensePlate +"' ," +
+        "'" + bike.manuafacturingDate +"' ," +
+        "'" + bike.producer +"' ," +
+        "'" + bike.cost +"' ," +
+        "'" + bike.idPark +"' ," +
+        "'" + bike.batteryPercentage +"' ," +
+        "'" + bike.loadCycles +"' ," +
+        "'" + bike.timeRemaining +"' )"
     :
             "INSERT INTO bike (name, type, weight,licensePlate, manuafacturingDate, producer, cost, idPark) VALUES" +
-            " ('"+ bike.name.toString() +"' ," +
-            "'" + bike.type.toString() +"' ," +
-            "'" + bike.weight.toString() +"' ," +
-            "'" + bike.licensePlate.toString() +"' ," +
-            "'" + bike.manuafacturingDate.toString() +"' ," +
-            "'" + bike.producer.toString() +"' ," +
-            "'" + bike.cost.toString() +"' ," +
-            "'" + bike.idPark.toString() +"' )"
+            " ('"+ bike.name +"' ," +
+            "'" + bike.type +"' ," +
+            "'" + bike.weight +"' ," +
+            "'" + bike.licensePlate +"' ," +
+            "'" + bike.manuafacturingDate +"' ," +
+            "'" + bike.producer +"' ," +
+            "'" + bike.cost +"' ," +
+            "'" + bike.idPark +"' )"
     ;
     return db.save(sql);
 }
