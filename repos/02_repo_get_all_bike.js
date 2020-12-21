d128f46c53b19a193e7204d98cff764faecc9139
quanganh991 <dinhqanh2012@gmail.com>
2020-12-21 17:33:15 +0700

    type -> typed

--- repos/02_repo_get_all_bike.js
+++ repos/02_repo_get_all_bike.js
@@ -1,5 +1,5 @@
 var db = require('../fn/db');
 exports.getAllBike = (id_station) => {
-    var sql = "select * from bike WHERE idPark = '" + id_station.toString();
+    var sql = "select * from bike WHERE idPark = " + id_station.toString();
     return db.load(sql);
 }
